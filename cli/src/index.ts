e4a4f5fe5184f8b88b29022af5c2335eb8ec0023
Jacob Paris <jacob@jacobparis.com>
2023-07-19 03:58:26 +0100

    fix: use envs for flags

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import "./env.js"
 
 import { add } from "~/src/commands/add.js"
 import { init } from "~/src/commands/init.js"
 import { Command } from "commander"
 import { restoreCache } from "./cache.js"
 import { refresh } from "./commands/refresh.js"
+import { libraryCommand } from "./commands/library.js"
 
 process.on("SIGINT", () => process.exit(0))
 process.on("SIGTERM", () => process.exit(0))
@@ -18,9 +18,24 @@ async function main() {
     .description(
       "add components, icons, and utilities as code, not dependencies"
     )
+    .option("-y, --yes", "skip confirmation prompt.", false)
+    // flags with --no are inverted, so this is yes-cache by default
+    .option("--no-cache", "disable caching.", true)
     .version("1.0.0", "-v, --version", "display the version number")
+    .hook("preAction", () => {
+      // This runs before every command, so this is our global state
+      const options = program.optsWithGlobals()
 
-  program.addCommand(init).addCommand(add).addCommand(refresh)
+      // Flags override env vars
+      process.env.YES = options.yes ? "true" : ""
+      process.env.CACHE = options.cache ? "true" : ""
+    })
+
+  program
+    .addCommand(init)
+    .addCommand(add)
+    .addCommand(refresh)
+    .addCommand(libraryCommand)
 
   program.parse()
 }
