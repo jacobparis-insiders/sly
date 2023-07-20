47e6d03a0417ee197a5585fec7fb7b1598a99298
Jacob Paris <jacob@jacobparis.com>
2023-07-20 07:35:51 +0100

    fix: surpass 1000 icon limit

--- site/app/routes/registry+/lucide-icons[.json].ts
+++ site/app/routes/registry+/lucide-icons[.json].ts
@@ -2,42 +2,58 @@
 // https://sly-cli.fly.dev/registry/lucide-icons.json
 
 import { json, type LoaderArgs } from "@remix-run/node"
-import { z } from "zod"
-import { githubFile, type libraryIndexSchema } from "../../schemas.js"
+import type { z } from "zod"
+import { type libraryIndexSchema } from "../../schemas.js"
+
+import { Octokit } from "octokit"
 
 export const meta = {
   name: "lucide-icons",
-  source:
-    "https://github.com/radix-ui/icons/tree/master/packages/radix-icons/icons",
+  source: "https://github.com/lucide-icons/lucide/tree/main/icons",
   description:
     "Community-run fork of Feather Icons, open for anyone to contribute icons.",
-  license: "https://github.com/radix-ui/icons/blob/master/LICENSE",
+  license: "https://github.com/lucide-icons/lucide/blob/main/LICENSE",
 } as const
 
 export async function loader({ request }: LoaderArgs) {
-  const lucide = await fetch(
-    "https://api.github.com/repos/lucide-icons/lucide/contents/icons"
-  )
-    .then((res) => res.json())
-    .then(z.array(githubFile).parseAsync)
+  const octokit = new Octokit()
 
-  const icons = lucide
-    .filter((icon) => {
-      if (icon.type !== "file") return false
-      if (!icon.path.endsWith(".svg")) return false
+  // get latest commit for directory path
+  const latestCommit = await octokit.rest.repos.getCommit({
+    owner: "lucide-icons",
+    repo: "lucide",
+    path: "icons",
+    ref: "main",
+  })
+
+  // get tree for latest commit
+  const tree = await octokit.rest.git.getTree({
+    owner: "lucide-icons",
+    repo: "lucide",
+    tree_sha: latestCommit.data.sha,
+    recursive: "true",
+  })
+
+  const files = tree.data.tree
+    .filter((file) => {
+      if (file.type !== "blob") return false
+      if (!file.path) return false
+      if (!file.path.endsWith(".svg")) return false
+      if (!file.path.startsWith("icons/")) return false
 
       return true
     })
-    .map((icon) => ({
-      name: icon.name.replace(/\.svg$/, ""),
-      dependencies: [],
-      devDependencies: [],
-      registryDependencies: [],
-    }))
+    .map((file) => {
+      if (!file.path) throw new Error("File path is undefined")
 
-  return json<z.infer<typeof libraryIndexSchema>>({
+      return {
+        name: file.path?.replace(/^icons\//, "").replace(/\.svg$/, ""),
+      }
+    })
+
+  return json<z.input<typeof libraryIndexSchema>>({
     version: "1.0.0",
     meta,
-    resources: icons,
+    resources: files,
   })
 }
