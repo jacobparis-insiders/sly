515158c9311f1c5ca29a570835c19c58ebf80d01
Jacob Paris <jacob@jacobparis.com>
2023-07-24 04:18:45 +0100

    fix: add version check

---
+++ cli/src/check-version.ts
@@ -0,0 +1,17 @@
+import { compareVersions } from "compare-versions"
+import { getRegistryIndex } from "./registry.js"
+import packageJson from "../package.json"
+import { logger } from "./logger.js"
+import chalk from "chalk"
+
+export function checkVersion() {
+  return getRegistryIndex().then(({ version }) => {
+    const comparison = compareVersions(version, packageJson.version)
+    if (comparison === 1) {
+      logger.warn(`Update available ${packageJson.version} -> ${version}.`)
+      logger.warn(
+        `Run ${chalk.bold("npm i --save-dev @sly-cli/sly")} to update.`
+      )
+    }
+  })
+}
--- cli/src/index.ts
+++ cli/src/index.ts
@@ -6,41 +6,37 @@ import { Command } from "commander"
 import { restoreCache } from "./cache.js"
 import { refresh } from "./commands/refresh.js"
 import { libraryCommand } from "./commands/library.js"
+import { checkVersion } from "./check-version.js"
 
 process.on("SIGINT", () => process.exit(0))
 process.on("SIGTERM", () => process.exit(0))
 
-async function main() {
-  void restoreCache()
+await checkVersion()
+void restoreCache()
 
-  const program = new Command()
-    .name("sly")
-    .description(
-      "add components, icons, and utilities as code, not dependencies"
-    )
-    .option("-y, --yes", "skip confirmation prompt.", false)
-    // flags with --no are inverted, so this is yes-cache by default
-    .option("--no-cache", "disable caching.", true)
-    .version("1.0.0", "-v, --version", "display the version number")
-    .hook("preAction", () => {
-      // This runs before every command, so this is our global state
-      const options = program.optsWithGlobals()
+const program = new Command()
+  .name("sly")
+  .description("add components, icons, and utilities as code, not dependencies")
+  .option("-y, --yes", "skip confirmation prompt.", false)
+  // flags with --no are inverted, so this is yes-cache by default
+  .option("--no-cache", "disable caching.", true)
+  .version("1.0.0", "-v, --version", "display the version number")
+  .hook("preAction", () => {
+    // This runs before every command, so this is our global state
+    const options = program.optsWithGlobals()
 
-      // Flags override env vars
-      process.env.YES = options.yes ? "true" : ""
-      process.env.CACHE = options.cache ? "true" : ""
-    })
+    // Flags override env vars
+    process.env.YES = options.yes ? "true" : ""
+    process.env.CACHE = options.cache ? "true" : ""
+  })
 
-  program
-    .addCommand(init)
-    .addCommand(add)
-    .addCommand(refresh)
-    .addCommand(libraryCommand)
+program
+  .addCommand(init)
+  .addCommand(add)
+  .addCommand(refresh)
+  .addCommand(libraryCommand)
 
-  program.parse()
-}
-
-main()
+program.parse()
 
 /**
  * These are imported by the JSDoc in the transformers
