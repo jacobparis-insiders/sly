b07c6c4646ff73ef33cd04a0a9cb346a925eed6a
Jacob Paris <jacob@jacobparis.com>
2023-07-24 04:19:47 +0100

    feature: show latest CLI version in registry

--- site/app/routes/registry+/index[.json].ts
+++ site/app/routes/registry+/index[.json].ts
@@ -2,7 +2,7 @@
 // https://sly-cli.fly.dev/registry/index.json
 
 import { json, type LoaderArgs } from "@remix-run/node"
-import type { z } from "zod"
+import { z } from "zod"
 import { meta as shadcnMeta } from "./@shadcn.ui[.json].js"
 import { meta as radixMeta } from "./@radix-ui.icons[.json].js"
 import { meta as lucideMeta } from "./lucide-icons[.json].js"
@@ -10,10 +10,30 @@ import { meta as transformersMeta } from "./@sly-cli.transformers[.json].js"
 import { meta as simpleIconsMeta } from "./simple-icons[.json].js"
 import { meta as heroiconsMeta } from "./tailwindlabs.heroicons[.json].js"
 import type { registryIndexSchema } from "../../schemas.js"
+import cachified from "cachified"
+import { cache } from "../../cache.server.js"
+
+const npmSchema = z.object({
+  "dist-tags": z.object({
+    latest: z.string(),
+  }),
+})
 
 export async function loader({ request }: LoaderArgs) {
+  const npm = await cachified({
+    key: `npm/@sly-cli/sly`,
+    cache,
+    staleWhileRevalidate: 1000 * 60 * 60 * 12, // 12 hours
+    ttl: 1000 * 60 * 60 * 12, // 12 hours
+    async getFreshValue() {
+      return fetch("https://registry.npmjs.org/@sly-cli/sly")
+        .then((response) => response.json())
+        .then((response) => npmSchema.parseAsync(response))
+    },
+  })
+
   return json<z.input<typeof registryIndexSchema>>({
-    version: "1.0.0",
+    version: npm["dist-tags"].latest,
     libraries: [
       heroiconsMeta,
       lucideMeta,
