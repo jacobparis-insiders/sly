4a83e0d8b80544c3d7b6ec3aa0c6fa5bbfeb8616
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2023-10-05 10:39:07 -0400

    fix transformers on Windows (#8)

--- cli/src/transformers.ts
+++ cli/src/transformers.ts
@@ -4,6 +4,7 @@ import { Transformer } from "./index.js"
 import { logger } from "~/src/logger.js"
 import { getConfigFilepath } from "./get-config.js"
 import chalk from "chalk"
+import { pathToFileURL } from "url";
 
 export async function resolveTransformers(transformers: string[]) {
   const modules = await Promise.all(
@@ -17,7 +18,9 @@ export async function resolveTransformers(transformers: string[]) {
         process.exit(1)
       }
 
-      return import(transformerPath).catch((error) => {
+      // shout out to next
+      // https://github.com/vercel/next.js/blob/b15a976e11bf1dc867c241a4c1734757427d609c/packages/next/server/config.ts#L748-L765
+      return import(pathToFileURL(transformerPath).href).catch((error) => {
         logger.error(`Failed to load transformer ${transformer}.`)
         if (error.code === "ERR_UNKNOWN_FILE_EXTENSION") {
           logger.warn(
