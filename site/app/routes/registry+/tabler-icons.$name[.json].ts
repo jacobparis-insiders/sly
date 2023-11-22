eee680c368b970678216908888608b0086b8430d
Hugo Meissner <meissnerhugo@gmail.com>
2023-11-22 05:55:48 +0100

    feature: add tabler/tabler-icons to registry (#9)

---
+++ site/app/routes/registry+/tabler-icons.$name[.json].ts
@@ -0,0 +1,43 @@
+// http://localhost:3000/registry/tabler-icons/12-hours.json
+// https://sly-cli.fly.dev/registry/tabler-icons/12-hours.json
+
+import { json, type LoaderArgs } from "@remix-run/node";
+import { meta } from "./tabler-icons[.json].js";
+import { type libraryItemWithContentSchema } from "../../schemas.js";
+import type { z } from "zod";
+import { getGithubFile } from "../../github.server.js";
+
+export async function loader({ params }: LoaderArgs) {
+  const icon = await getGithubFile({
+    owner: "tabler",
+    repo: "tabler-icons",
+    path: `icons/${params.name}.svg`,
+    ref: "master",
+  });
+
+  if (!icon) {
+    throw new Response("Not found", { status: 404 });
+  }
+
+  if (!icon.download_url) {
+    throw new Response("Not found", { status: 404 });
+  }
+
+  return json<z.input<typeof libraryItemWithContentSchema>>({
+    name: icon.name.replace(/\.svg$/, ""),
+    meta: {
+      ...meta,
+      source: icon.html_url ?? meta.source,
+    },
+    files: [
+      {
+        name: icon.name,
+        content: [
+          `<!-- ${meta.name} -->`,
+          `<!-- ${meta.license} -->`,
+          await fetch(icon.download_url).then((res) => res.text()),
+        ].join("\n"),
+      },
+    ],
+  });
+}
---
+++ site/app/routes/registry+/tabler-icons[.json].ts
@@ -0,0 +1,43 @@
+// http://localhost:3000/registry/tabler-icons/icons.json
+// https://sly-cli.fly.dev/registry/tabler-icons/icons.json
+
+import { json, type LoaderArgs } from "@remix-run/node";
+import type { z } from "zod";
+import { type libraryIndexSchema } from "../../schemas.js";
+import { getGithubDirectory } from "../../github.server.js";
+
+export const meta = {
+  name: "tabler-icons",
+  source: "https://github.com/tabler/tabler-icons/tree/master/icons",
+  description: "A set of over 4800 free MIT-licensed high-quality SVG icons.",
+  license: "https://github.com/tabler/tabler-icons/blob/master/LICENSE",
+} as const;
+
+export async function loader({ request }: LoaderArgs) {
+  const files = await getGithubDirectory({
+    owner: "tabler",
+    repo: "tabler-icons",
+    path: "icons",
+    ref: "master",
+  });
+
+  const resources = files
+    .filter((file) => {
+      if (!file.path?.endsWith(".svg")) return false;
+
+      return true;
+    })
+    .map((file) => {
+      if (!file.path) throw new Error("File path is undefined");
+
+      return {
+        name: file.path?.replace(/^icons\//, "").replace(/\.svg$/, ""),
+      };
+    });
+
+  return json<z.input<typeof libraryIndexSchema>>({
+    version: "1.0.0",
+    meta,
+    resources,
+  });
+}
