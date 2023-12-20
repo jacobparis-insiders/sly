c0ae93d3d4121393abcee06fa14839b53ea35637
Jacob Paris <jacob@jacobparis.com>
2023-12-20 22:42:08 +0700

    fix: update test timeout

--- cli/vitest.config.ts
+++ cli/vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
+    // CLI tests are against the live registry, so they need a longer timeout
+    testTimeout: 10000,
     // run tests in sequence instead of parallel
     chaiConfig: {
       showDiff: false,
