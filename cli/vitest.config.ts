8ff08a800aeeea73fdc8c5033e3c425942968911
Jacob Paris <jacob@jacobparis.com>
2023-12-20 16:57:32 +0700

    fix: use hosted registry for tests

---
+++ cli/vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    // run tests in sequence instead of parallel
+    chaiConfig: {
+      showDiff: false,
+      truncateThreshold: 1000,
+    },
+    bail: 1,
+  },
+})
