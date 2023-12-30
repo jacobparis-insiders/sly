ceb53438411d4411a2755adf2a20ac99b77b1595
jolbol1 <github@jamesshopland.com>
2023-12-30 12:11:30 +0000

    feat: add draft-ui to registry

---
+++ site/app/routes/registry+/draft-ui.$name[.json].ts
@@ -0,0 +1,44 @@
+// http://localhost:3000/registry/draft-ui/breadcrumbs.json
+// https://sly-cli.fly.dev/registry/draft-ui/breadcrumbs.json
+
+import { json, type LoaderFunctionArgs } from "@remix-run/node"
+
+import { meta } from "./draft-ui[.json]"
+import { type libraryItemWithContentSchema } from "../../schemas.js"
+import type { z } from "zod"
+import { getGithubFile } from "../../github.server.js"
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const file = await getGithubFile({
+    owner: "IHIutch",
+    repo: "draft-ui",
+    path: `packages/ui/src/${params.name}.tsx`,
+    ref: "main",
+  });
+
+  if (!file) {
+    throw new Response("Not found", { status: 404 })
+  }
+
+  if (!file.download_url) {
+    throw new Response("Not found", { status: 404 })
+  }
+
+  return json<z.input<typeof libraryItemWithContentSchema>>({
+    name: file.name,
+    meta: {
+      ...meta,
+      source: file.html_url ?? meta.source,
+    },
+    files: [
+      {
+        name: file.name,
+        content: [
+          `// ${meta.name}`,
+          `// ${meta.license}`,
+          await fetch(file.download_url).then((res) => res.text()),
+        ].join("\n"),
+      },
+    ],
+  })
+}
