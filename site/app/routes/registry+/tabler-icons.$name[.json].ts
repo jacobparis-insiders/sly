c3f2ca0ba642a8dbfd16e86f0ed867c29d4c8b06
Jacob Paris <jacob@jacobparis.com>
2024-03-06 18:25:14 +0200

    feature: update tabler icons to variants

--- site/app/routes/registry+/tabler-icons.$name[.json].ts
+++ site/app/routes/registry+/tabler-icons.$name[.json].ts
@@ -8,11 +8,16 @@ import type { z } from "zod"
 import { getGithubFile } from "../../github.server.js"
 
 export async function loader({ params }: LoaderFunctionArgs) {
+  const variant = params.name?.endsWith("-outline") ? "outline" : "filled"
+  const trimmedName = params.name
+    ?.replace(/-outline$/, "")
+    .replace(/-filled$/, "")
+
   const icon = await getGithubFile({
     owner: "tabler",
     repo: "tabler-icons",
-    path: `icons/${params.name}.svg`,
-    ref: "master",
+    path: `icons/${variant}/${trimmedName}.svg`,
+    ref: "main",
   })
 
   if (!icon) {
@@ -24,14 +29,14 @@ export async function loader({ params }: LoaderFunctionArgs) {
   }
 
   return json<z.input<typeof libraryItemWithContentSchema>>({
-    name: icon.name.replace(/\.svg$/, ""),
+    name: icon.name.replace(/\.svg$/, `-${variant}`),
     meta: {
       ...meta,
       source: icon.html_url ?? meta.source,
     },
     files: [
       {
-        name: icon.name,
+        name: icon.name.replace(/\.svg$/, `-${variant}.svg`),
         content: [
           `<!-- ${meta.name} -->`,
           `<!-- ${meta.license} -->`,
