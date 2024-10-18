30dc1296c6569b1f9f12dd2b99e7ae6f74c30416
Jacob Paris <jacob@jacobparis.com>
2024-10-18 21:01:54 +0800

    fix: Check npm version directly

--- cli/src/check-version.ts
+++ cli/src/check-version.ts
@@ -1,17 +1,21 @@
 import { compareVersions } from "compare-versions"
-import { getRegistryIndex } from "./registry.js"
 import packageJson from "../package.json"
 import { logger } from "./logger.js"
 import chalk from "chalk"
+import { npmSchema } from "site/app/schemas.js"
 
-export function checkVersion() {
-  return getRegistryIndex().then(({ version }) => {
-    const comparison = compareVersions(version, packageJson.version)
-    if (comparison === 1) {
-      logger.warn(`Update available ${packageJson.version} -> ${version}.`)
-      logger.warn(
-        `Run ${chalk.bold("npm i --save-dev @sly-cli/sly")} to update.`,
-      )
-    }
-  })
+export async function checkVersion() {
+  return fetch("https://registry.npmjs.org/@sly-cli/sly")
+    .then((response) => response.json())
+    .then((response) => npmSchema.parseAsync(response))
+    .then((npm) => {
+      const version = npm["dist-tags"].latest
+      const comparison = compareVersions(version, packageJson.version)
+      if (comparison === 1) {
+        logger.warn(`Update available ${packageJson.version} -> ${version}.`)
+        logger.warn(
+          `Run ${chalk.bold("npm i --save-dev @sly-cli/sly")} to update.`,
+        )
+      }
+    })
 }
